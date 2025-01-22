4872939894ba2aa6f9d0c5c62d9d3e3e5bd9c144
a-tommyyy <a-akazawa@mercari.com>
2025-01-22 23:52:56 +0900

    fix error cannot find setHistory

--- src/components/terminal/cmd/useCmd.ts
+++ src/components/terminal/cmd/useCmd.ts
@@ -5,7 +5,7 @@ type UseCmd = {
   formRef: React.RefObject<HTMLFormElement>;
   handleExecCmd: (input: string) => void;
   commandProps: CmdProps[];
-}
+};
 
 export const useCmd = (): UseCmd => {
   // To scroll to bottom of terminal when new command is entered
@@ -16,23 +16,24 @@ export const useCmd = (): UseCmd => {
   const [results, setResults] = useState<CmdProps[]>([]);
 
   const handleExecCmd = (input: string) => {
-    const command = input.split(' ')[0];
-    const args = input.split(' ').slice(1);
-    if (command === 'clear') {
-      return setHistory([])
+    const command = input.split(" ")[0];
+    const args = input.split(" ").slice(1);
+    if (command === "clear") {
+      return setResults([]);
     }
 
     const result = {
       rawInput: input,
       command,
       args,
-    }
-    setResults(results=> [...results, result])
-  }
+    };
+    setResults((results) => [...results, result]);
+  };
 
   return {
     formRef,
     handleExecCmd,
     commandProps: results,
-  }
-}
+  };
+};
+
